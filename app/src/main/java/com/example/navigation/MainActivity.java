657165d287f9293af29db0f1102170ca9bd5787c
FastTrack <FastTrack@DESKTOP-2SN9M6U>
2020-02-03 10:32:10 +0200

    bottom navigation using navigation graph

--- app/src/main/java/com/example/navigation/MainActivity.java
+++ app/src/main/java/com/example/navigation/MainActivity.java
@@ -5,7 +5,10 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentTransaction;
+import androidx.navigation.NavController;
 import androidx.navigation.Navigation;
+import androidx.navigation.fragment.NavHostFragment;
+import androidx.navigation.ui.NavigationUI;
 
 import android.os.Bundle;
 import android.util.Log;
@@ -24,7 +27,8 @@ public class MainActivity extends AppCompatActivity {
     BottomNavigationView bottomNavigation;
     Toolbar toolbarTop;
     private static final String TAG = "MainActivity";
- TextView textView;
+    TextView textView;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -33,66 +37,72 @@ public class MainActivity extends AppCompatActivity {
         setSupportActionBar(toolbarTop);
         //toolbarTop.setTitle("");
         MainActivity.this.setTitle("");
-        textView=findViewById(R.id.title);
+        textView = findViewById(R.id.title);
 
+        BottomNavigationView bottomNavigationView = findViewById(R.id.bottom_navigation);
+        NavHostFragment navHostFragment = (NavHostFragment) getSupportFragmentManager()
+                .findFragmentById(R.id.mainNavigationFragment);
+        NavigationUI.setupWithNavController(bottomNavigationView,
+                navHostFragment.getNavController());
 
-
-        setDefaultFragment();
-
-
-        bottomNavigation = findViewById(R.id.bottom_navigation);
-        bottomNavigation.setSelectedItemId(R.id.homeFragment);
-
-        bottomNavigation.setOnNavigationItemSelectedListener(navigationItemSelectedListener);
-
-    }
-
-    BottomNavigationView.OnNavigationItemSelectedListener navigationItemSelectedListener =
-            new BottomNavigationView.OnNavigationItemSelectedListener() {
-
-                @Override
-                public boolean onNavigationItemSelected(@NonNull MenuItem item) {
-                    switch (item.getItemId()) {
-                        case R.id.shopsFragment:
-                            textView.setText("Shops");
-                            openFragment(new ShopsFragment());
-                            Log.e(TAG, "onNavigationItemSelected: ");
-                            return true;
-                        case R.id.ordersFragment:
-                            textView.setText("Orders");
-                            openFragment(new OrdersFragment());
-                            return true;
-                        case R.id.homeFragment:
-                            textView.setText("Home");
-
-                            openFragment(new HomeFragment());
-
-                            return true;
-                        case R.id.sittingsFragment:
-                            textView.setText("Sittings");
-
-                            openFragment(new SittingsFragment());
-                            return true;
-                        case R.id.profileFragment:
-                            textView.setText("Profile");
-
-                            openFragment(new ProfileFragment());
-                            return true;
-                    }
-                    return false;
-                }
-            };
-
-    public void openFragment(Fragment fragment) {
-        FragmentTransaction transaction = getSupportFragmentManager().beginTransaction();
-        transaction.replace(R.id.container, fragment);
-        transaction.addToBackStack(null);
-        transaction.commit();
-    }
-    private void setDefaultFragment() {
-        FragmentTransaction transaction = getSupportFragmentManager().beginTransaction();
-        transaction.replace(R.id.container,new HomeFragment());
-        transaction.commit();
-
+//
+//        setDefaultFragment();
+//
+//
+//        bottomNavigation = findViewById(R.id.bottom_navigation);
+//        bottomNavigation.setSelectedItemId(R.id.homeFragment);
+//
+//        bottomNavigation.setOnNavigationItemSelectedListener(navigationItemSelectedListener);
+//
+//    }
+//
+//    BottomNavigationView.OnNavigationItemSelectedListener navigationItemSelectedListener =
+//            new BottomNavigationView.OnNavigationItemSelectedListener() {
+//
+//                @Override
+//                public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+//                    switch (item.getItemId()) {
+//                        case R.id.shopsFragment:
+//                            textView.setText("Shops");
+//                            openFragment(new ShopsFragment());
+//                            Log.e(TAG, "onNavigationItemSelected: ");
+//                            return true;
+//                        case R.id.ordersFragment:
+//                            textView.setText("Orders");
+//                            openFragment(new OrdersFragment());
+//                            return true;
+//                        case R.id.homeFragment:
+//                            textView.setText("Home");
+//
+//                            openFragment(new HomeFragment());
+//
+//                            return true;
+//                        case R.id.sittingsFragment:
+//                            textView.setText("Sittings");
+//
+//                            openFragment(new SittingsFragment());
+//                            return true;
+//                        case R.id.profileFragment:
+//                            textView.setText("Profile");
+//
+//                            openFragment(new ProfileFragment());
+//                            return true;
+//                    }
+//                    return false;
+//                }
+//            };
+//
+//    public void openFragment(Fragment fragment) {
+//        FragmentTransaction transaction = getSupportFragmentManager().beginTransaction();
+//        transaction.replace(R.id.container, fragment);
+//        transaction.addToBackStack(null);
+//        transaction.commit();
+//    }
+//    private void setDefaultFragment() {
+//        FragmentTransaction transaction = getSupportFragmentManager().beginTransaction();
+//        transaction.replace(R.id.container,new HomeFragment());
+//        transaction.commit();
+//
+//    }
     }
 }
